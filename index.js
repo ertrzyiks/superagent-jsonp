9a7583349debb417c327600f22ea1cea39aca376
Mateusz Derks <mateusz.derks@schibsted.pl>
2015-04-09 16:47:36 +0200

    Fix for non jsonp requests

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ module.exports = function(superagent) {
   var Request = superagent.Request;
 
   Request.prototype.jsonp = jsonp;
+  Request.prototype._originalEnd = Request.prototype.end;
   Request.prototype.end = end;
 
   return superagent;
@@ -38,6 +39,10 @@ var jsonp = function(options){
 
 var end = function(callback){
   this.callback = callback;
+
+  if (!this.options) {
+    return this._originalEnd(callback);
+  }
   var params = {},
     paramName = this.options.paramName || 'callback';
 
